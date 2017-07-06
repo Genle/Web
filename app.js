71465f1df0fa73fc14d172f3be3c285dfdbf5772
Genle <haitech1994@gmail.com>
2017-07-06 12:39:03 -0400

    "Mongodb and .env set up"

--- app.js
+++ app.js
@@ -3,30 +3,49 @@
     let handlebar = require('express-handlebars');
     let helpers = require('./lib/helpers');
     let app = express();
+    let db = require("./db");
 
-    app.use('/static', express.static('public')); 
-    app.engine('.hb', handlebar({defaultLayout: 'main',
-                                extname: '.hb',
-                                layoutsDir:__dirname + '/views/layouts',
-                                partialsDir:__dirname + '/views/partials',
-                                helpers: helpers }));
+    // db.saveUser('joe', 'milipap');
+    db.getUserInfo();
+    console.log(db);
+
+    app.use('/static', express.static('public'));
+    app.engine('.hb', handlebar({
+        defaultLayout: 'main',
+        extname: '.hb',
+        layoutsDir: __dirname + '/views/layouts',
+        partialsDir: __dirname + '/views/partials',
+        helpers: helpers
+    }));
     app.set('view engine', '.hb');
 
 
+    //data
+    let users = [
+        {username: "jfageek", password: "lamelma"},
+        {username: "angelo",password: "lama"},
+        {username: "joseph", password: "french"}
+        ];
 
     //endpoint
+
 
     app.get('/', (req, res) => {
         res.render('home');
     });
 
-    app.get('/about', (req,res) => {
+    app.get('/about', (req, res) => {
         res.render('about');
     });
 
     app.get('/events', (req, res) => {
         res.sendFile(__dirname + '/views/events.html');
-    })
+    });
+
+    app.get('/ingredient', (req, res) => {
+        res.send(users);
+
+    });
 
     let port = 9876; //process.env.PORT ||
 
@@ -34,4 +53,4 @@
         console.log("App is listening to port 9876");
     });
 
-} () )
+}() );
